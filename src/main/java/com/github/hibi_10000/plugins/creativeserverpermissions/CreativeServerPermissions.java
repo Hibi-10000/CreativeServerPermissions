f1cb21e329f947ccb9b2aef4ff27e10349b1fcd4
hibi-10000 <hibi_10000@outlook.jp>
2022-05-15 21:58:03 +0900

    Add README.md

--- src/main/java/com/github/hibi_10000/plugins/creativeserverpermissions/CreativeServerPermissions.java
+++ src/main/java/com/github/hibi_10000/plugins/creativeserverpermissions/CreativeServerPermissions.java
@@ -1,125 +1,296 @@
-package com.github.hibi_10000.plugin.creativeserverpermissions;
+package com.github.hibi_10000.plugins.creativeserverpermissions;
 
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
+import org.bukkit.*;
 import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 
 public class CreativeServerPermissions extends JavaPlugin {
 
-	@Override
-	public void onEnable() {
-
+	private void sendChangeGamemodeMessage(CommandSender sender, Player player, GameMode gamemode) {
+		if (player == null) {
+			sender.sendMessage("Set own game mode to " + upperCaseFirst(gamemode.name().toLowerCase()) + " Mode");
+			Bukkit.getLogger().log(Level.INFO, ChatColor.GRAY + "[" + sender.getName() + ": Set own game mode to " + upperCaseFirst(gamemode.name().toLowerCase()) + " Mode]");
+			return;
+		}
+		sender.sendMessage("Set " + player.getName() + " game mode to " + upperCaseFirst(gamemode.name().toLowerCase()) + " Mode");
+		player.sendMessage("Your game mode has been updated to " + upperCaseFirst(gamemode.name().toLowerCase()) + " Mode");
+		Bukkit.getLogger().log(Level.INFO, ChatColor.GRAY + "[" + sender.getName() + ": Set " + player.getName() + " game mode to " + upperCaseFirst(gamemode.name().toLowerCase()) + " Mode]");
 	}
 
-	@Override
-	public void onDisable() {
+	private void teleport(CommandSender sender, Player target, Player toplayer, Location to) {
+		if (to == null && toplayer != null) {
+			target.teleport(toplayer);
+			sender.sendMessage("Teleported " + target.getName() + " to " + toplayer.getName());
+			Bukkit.getLogger().log(Level.INFO, ChatColor.GRAY + "[" + sender.getName() + ": Teleported " + target.getName() + " to " + toplayer.getName() + "]");
+			if (target != sender) {
+				target.sendMessage(ChatColor.GRAY + "[" + sender.getName() + ": Teleported " + target.getName() + " to " + toplayer.getName() + "]");
+			}
+		} else if (toplayer == null && to != null) {
+			target.teleport(to);
+			sender.sendMessage("Teleported " + target.getName() + " to " + to.getX() + ", " + to.getY() + ", " + to.getZ());
+			Bukkit.getLogger().log(Level.INFO, ChatColor.GRAY + "[" + sender.getName() + ": Teleported " + target.getName() + " to " + to.getX() + ", " + to.getY() + ", " + to.getZ() + "]");
+			if (target != sender) {
+				target.sendMessage(ChatColor.GRAY + "[" + sender.getName() + ": Teleported " + target.getName() + " to " + to.getX() + ", " + to.getY() + ", " + to.getZ() + "]");
+			}
+		}
+	}
 
+	private Location getLocation(CommandSender sender, String x, String y, String z, Float pitch, Float yaw) {
+		Player p = (Player) sender;
+		try {
+			if (x.startsWith("~")) {
+				if (x.equalsIgnoreCase("~")) {
+					x = String.valueOf(p.getLocation().getX());
+				} else {
+					x = String.valueOf((p.getLocation().clone().add(Double.parseDouble(x.replaceFirst("~", "")), 0, 0).getX()));
+				}
+			}
+			if (y.startsWith("~")) {
+				if (y.equalsIgnoreCase("~")) {
+					y = String.valueOf(p.getLocation().getY());
+				} else {
+					y = String.valueOf(p.getLocation().clone().add(0, Double.parseDouble(y.replaceFirst("~", "")), 0).getY());
+				}
+			}
+			if (z.startsWith("~")) {
+				if (z.equalsIgnoreCase("~")) {
+					z = String.valueOf(p.getLocation().getZ());
+				} else {
+					z = String.valueOf(p.getLocation().clone().add(0, 0, Double.parseDouble(z.replaceFirst("~", ""))).getZ());
+				}
+			}
+		} catch (NumberFormatException e) {
+			sender.sendMessage(ChatColor.RED + "Incorrect argument for command");
+		}
+		return new Location(p.getWorld(), Double.parseDouble(x), Double.parseDouble(y), Double.parseDouble(z), yaw, pitch);
+	}
+
+	public static String upperCaseFirst(String val) {
+		char[] arr = val.toCharArray();
+		arr[0] = Character.toUpperCase(arr[0]);
+		return new String(arr);
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		Player p = (Player) sender;
+		//Player p = (Player) sender;
 		if (command.getName().equalsIgnoreCase("gamemode")) {
+			if (sender instanceof ConsoleCommandSender || sender instanceof BlockCommandSender || sender.hasPermission("minecraft.command.gamemode")) {
+				StringBuilder sb = new StringBuilder("minecraft:gamemode");
+				for (String str : args) {
+					sb.append(" ").append(str);
+				}
+				Bukkit.dispatchCommand(sender, sb.toString());
+				return true;
+			}
 			if (args.length == 0 || args.length > 2) {
-				sender.sendMessage("§cUnknown or incomplete command.");
+				sender.sendMessage(ChatColor.RED + "Unknown or incomplete command.");
 				return false;
 			}
 			GameMode gamemode;
 			try {
-				gamemode = GameMode.valueOf(args[0]);
+				gamemode = GameMode.valueOf(args[0].toUpperCase());
 			} catch (IllegalArgumentException e) {
-				sender.sendMessage("§cIncorrect argument for command");
+				sender.sendMessage(ChatColor.RED + "Incorrect argument for command");
 				return false;
 			}
 			if (sender.hasPermission("creativeserverpermissions.gamemode.other") && args.length == 2) {
 				if (args[1].equalsIgnoreCase("@e")) {
-					sender.sendMessage("§cOnly players may be affected by this command, but the provided selector includes entities");
+					sender.sendMessage(ChatColor.RED + "Only players may be affected by this command, but the provided selector includes entities");
 					return false;
 				} else if (args[1].equalsIgnoreCase("@s")) {
-					p.setGameMode(gamemode);
+					((Player) sender).setGameMode(gamemode);
+					sendChangeGamemodeMessage(sender, null, gamemode);
 					return true;
-				} else if (args[1].equalsIgnoreCase("@p") || args[1].equalsIgnoreCase("@a") || args[1].equalsIgnoreCase("@r")) {
+				} else if (args[1].equalsIgnoreCase("@a") || args[1].equalsIgnoreCase("@r")) {
 					for (Entity e : Bukkit.selectEntities(sender, args[1])) {
 						((Player) e).setGameMode(gamemode);
+						sendChangeGamemodeMessage(sender, (Player) e, gamemode);
 					}
 					return true;
 				}
-				try {
-					Bukkit.getPlayer(args[1]).setGameMode(gamemode);
-				} catch (NullPointerException e) {
-					sender.sendMessage("§cNo player was found");
-					return false;
+				Player target = Bukkit.getPlayer(args[1]);
+				if (target != null) {
+					target.setGameMode(gamemode);
+					sendChangeGamemodeMessage(sender, target, gamemode);
+					return true;
 				}
-				return true;
-			}
-			p.setGameMode(gamemode);
-			return true;
-		}
-		if (command.getName().equalsIgnoreCase("tp")) {
-			if (args.length == 0 || args.length > 6) {
-				sender.sendMessage("§cIncorrect argument for command");
+				sender.sendMessage(ChatColor.RED + "No player was found");
 				return false;
 			}
-			if (sender.hasPermission("creativeserverpermissions.teleport.other") || sender instanceof BlockCommandSender) {
+			((Player) sender).setGameMode(gamemode);
+			sendChangeGamemodeMessage(sender, null, gamemode);
+			return true;
+		}
+
+		if (command.getName().equalsIgnoreCase("tp")) {
+			if (sender instanceof ConsoleCommandSender || sender instanceof BlockCommandSender || sender.hasPermission("minecraft.command.teleport")) {
 				StringBuilder sb = new StringBuilder("minecraft:tp");
 				for (String str : args) {
 					sb.append(" ").append(str);
 				}
-				p.performCommand(sb.toString());
+				Bukkit.dispatchCommand(sender, sb.toString());
 				return true;
+			}
+			if (args.length == 0 || args.length > 6) {
+				sender.sendMessage(ChatColor.RED + "Incorrect argument for command");
+				return false;
+			}
+			if (sender.hasPermission("creativeserverpermissions.teleport.other")) {
+				for (Player target : Bukkit.getOnlinePlayers()) {
+					if (!target.getName().equalsIgnoreCase(sender.getName()) && target.getName().equalsIgnoreCase(args[0])) {
+						if (args.length == 2) {
+							try {
+								teleport(sender, target, Bukkit.getPlayer(args[1]), null);
+								return true;
+							} catch (NullPointerException e) {
+								sender.sendMessage(ChatColor.RED + "No player was found");
+								return false;
+							}
+						}
+						if (args.length == 4) {
+							teleport(sender, target, null, getLocation(sender, args[1], args[2], args[3], target.getLocation().getPitch(), target.getLocation().getYaw()));
+							return true;
+						}
+						if (args.length == 6) {
+							teleport(sender, target, null, getLocation(sender, args[1], args[2], args[3], Float.parseFloat(args[4]), Float.parseFloat(args[5])));
+							return true;
+						}
+						sender.sendMessage(ChatColor.RED + "Incorrect argument for command");
+						return false;
+					}
+				}
 			}
 			if (args[0].equalsIgnoreCase("@s") || args[0].equalsIgnoreCase("@p")) {
 				if (args.length == 2) {
 					try {
-						p.teleport(Bukkit.getPlayer(args[1]));
+						teleport(sender, (Player) sender, Bukkit.getPlayer(args[1]), null);
 						return true;
 					} catch (NullPointerException e) {
-						sender.sendMessage("§cNo player was found");
+						sender.sendMessage(ChatColor.RED + "No player was found");
 						return false;
 					}
 				}
 				if (args.length == 4) {
-					//String args1 = args[1];
-					//String args2 = args[2];
-					//String args3 = args[3];
-					//if (args[1].contains("~")) {
-					//	args1 = String.valueOf(p.getLocation().clone().add(Integer.parseInt(args1.replace("~","")), 0, 0);
-					//}
-					//Location targetloc = new Location(p.getWorld(), Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]);
-					StringBuilder sb = new StringBuilder("minecraft:tp");
-					for (String str : args) {
-						sb.append(" ").append(str);
-					}
-					p.performCommand(sb.toString());
+					teleport(sender, (Player) sender, null, getLocation(sender, args[1], args[2], args[3], ((Player) sender).getLocation().getPitch(), ((Player) sender).getLocation().getYaw()));
 					return true;
 				}
 				if (args.length == 6) {
-					StringBuilder sb = new StringBuilder("minecraft:tp");
-					for (String str : args) {
-						sb.append(" ").append(str);
-					}
-					p.performCommand(sb.toString());
+					teleport(sender, (Player) sender, null, getLocation(sender, args[1], args[2], args[3], Float.parseFloat(args[4]), Float.parseFloat(args[5])));
 					return true;
 				}
-				sender.sendMessage("Incorrect argument for command");
+				sender.sendMessage(ChatColor.RED + "Incorrect argument for command");
 				return false;
 			}
-
+			if (args.length == 1) {
+				try {
+					teleport(sender, (Player) sender, Bukkit.getPlayer(args[0]), null);
+					return true;
+				} catch (NullPointerException e) {
+					sender.sendMessage(ChatColor.RED + "No player was found");
+					return false;
+				}
+			}
+			if (args.length == 3) {
+				teleport(sender, (Player) sender, null, getLocation(sender, args[0], args[1], args[2], ((Player) sender).getLocation().getPitch(), ((Player) sender).getLocation().getYaw()));
+				return true;
+			}
+			if (args.length == 5) {
+				teleport(sender, (Player) sender, null, getLocation(sender, args[0], args[1], args[2], Float.parseFloat(args[3]), Float.parseFloat(args[4])));
+				return true;
+			}
+			sender.sendMessage(ChatColor.RED + "Incorrect argument for command");
+			return false;
 		}
 		return false;
 	}
 
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+		if (command.getName().equalsIgnoreCase("gamemode")) {
+			if (args.length == 1) {
+				List<String> list = new ArrayList<>();
+				 for (GameMode gm : GameMode.values()) {
+					 list.add(gm.name().toLowerCase());
+				 }
+				 return list;
+			} else if (args.length == 2 && sender.hasPermission("creativeserverpermissions.gamemode.other")) {
+				List<String> list = new ArrayList<>();
+				list.add("@a");
+				list.add("@e");
+				list.add("@p");
+				list.add("@r");
+				list.add("@s");
+				for (Player p : Bukkit.getOnlinePlayers()) {
+					list.add(p.getName());
+				}
+				return list;
+			}
+			return null;
+		}
 
+		if (command.getName().equalsIgnoreCase("tp")) {
+			List<String> plist = new ArrayList<>();
+			plist.add("@a");
+			plist.add("@e");
+			plist.add("@p");
+			plist.add("@r");
+			plist.add("@s");
+			for (Player p : Bukkit.getOnlinePlayers()) {
+				plist.add(p.getName());
+			}
+			if (args.length == 1) {
+				List<String> list = new ArrayList<>();
+				list.add("@a");
+				list.add("@e");
+				list.add("@p");
+				list.add("@r");
+				list.add("@s");
+				list.add("~");
+				list.add("~ ~");
+				list.add("~ ~ ~");
+				for (Player p : Bukkit.getOnlinePlayers()) {
+					list.add(p.getName());
+				}
+				return list;
+			}
+			if (args.length == 2 && sender.hasPermission("creativeserverpermissions.teleport.other") && plist.contains(args[0])) {
+				List<String> list = new ArrayList<>();
+				list.add("@a");
+				list.add("@e");
+				list.add("@p");
+				list.add("@r");
+				list.add("@s");
+				list.add("~");
+				list.add("~ ~");
+				list.add("~ ~ ~");
+				for (Player p : Bukkit.getOnlinePlayers()) {
+					list.add(p.getName());
+				}
+				return list;
+			}
+			if (args.length == 2 && args[0].contains("~")) {
+				List<String> list = new ArrayList<>();
+				list.add("~");
+				list.add("~ ~");
+				return list;
+			}
+			if (args.length == 3 && args[0].contains("~")) {
+				List<String> list = new ArrayList<>();
+				list.add("~");
+				return list;
+			}
+			return null;
+		}
 		return null;
 	}
 }
